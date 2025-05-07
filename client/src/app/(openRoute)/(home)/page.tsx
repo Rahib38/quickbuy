7b5b2af98bfdc7cfa8b169b59a2e18458a3efd48
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-05-07 11:20:55 -0700

    added

--- client/src/app/(openRoute)/(home)/page.tsx
+++ client/src/app/(openRoute)/(home)/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-<div>
-<Button>Click me</Button>
-</div>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/Basics/Navbar";
+import { Footer } from "@/components/Basics/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar ></Navbar>
+        <main className="min-h-[calc(100vh-463px)] my-14 container mx-auto px-10">{children}</main>
+        <Footer></Footer>
       </body>
     </html>
   );
